62921bb561c006f84c750c7d2c9b2d7bb3612c22
Andrey Fits <andreyfits@gmail.com>
2023-01-28 01:19:53 +0300

    feat: show posts on the main page

--- wp/wp-content/themes/simple-theme/index.php
+++ wp/wp-content/themes/simple-theme/index.php
@@ -1,5 +1,22 @@
 <?php get_header(); ?>
 
-<h1>Hello!</h1>
+    <div class="container">
+        <div class="row">
+            <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+                <div class="col-md-12">
+                    <div class="card">
+                        <img src="" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title"><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h5>
+                            <p class="card-text"><?php the_excerpt() ?></p>
+                            <a href="<?php the_permalink() ?>" class="btn btn-primary">Read</a>
+                        </div>
+                    </div>
+                </div>
+            <?php endwhile; else : ?>
+                <p><?php esc_html_e('Sorry, no posts matched your criteria.'); ?></p>
+            <?php endif; ?>
+        </div>
+    </div>
 
 <?php get_footer() ?>
